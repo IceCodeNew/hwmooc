cde2d2be69e53ae8da709488cbdd30e75e71008d
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2018-08-17 05:23:00 +0800

    Update
    使脚本能适应网站当前的情况。

--- out.py
+++ out.py
@@ -21,28 +21,28 @@ print("|    3.对获取视频下载地址增加可选的延时                  
 print("|    4.使用IE10的User-agent进行访问                            |")
 print("|                                                              |")
 print(" -------------------------------------------------------------- \n\n")
-kv = {"user-agent":"Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)"}
+kv = {"user-agent": "Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)"}
 pku = input("\n输入pku_auth:")
-a = {"pku_auth":str(pku)}
+a = {"pku_auth": str(pku)}
 print("\nCookies确认")
 print(a)
-import requests
 import re
 import time
+
+import requests
+
 liveid = input("输入课程任意ID:")
 print("\n正在分析课程信息")
-scurl = "http://www.chinesemooc.org/live/"+liveid
-try :
-    sc = requests.get(scurl,cookies = a,timeout = 15,headers = kv)
+scurl = "http://www.chinesemooc.org/live/" + liveid
+try:
+    sc = requests.get(scurl, cookies=a, timeout=15, headers=kv)
     sc.raise_for_status()
     sc.encoding = 'utf-8'
 except:
     print("Error：网络连接超时")
 
-
-
 try:
-    scname = re.search(r'《".*?》',sc.text)[0]
+    scname = re.search(r'《".*?》', sc.text)[0]
     scname = scname[4:-4]
     print("\n发现课程：")
     print(scname)
@@ -52,52 +52,57 @@ print("输出文件预处理...")
 try:
     olist = []
     with open('data.csv', 'wb') as f:
-        title = ["章节","标题","下载链接"]
+        title = ["章节", "下载链接"]
         line = ','.join(title) + '\n'
         f.write(line.encode('UTF-8'))
 except:
     print("创建数据文件失败，可能没有信息会被保存")
+
 de = input("\n准备就绪\n为避免服务器拒绝访问，每个视频下载链接获取成功后可选3秒延时\n输入大写字母N按回车来启用延时，否则请直接按回车开始")
 print("\n开始处理数据,请耐心等待\n")
-url = "http://www.chinesemooc.org/live/"+liveid
-r = requests.get(url,cookies = a,timeout = 15,headers = kv)
+url = "http://www.chinesemooc.org/live/" + liveid
+r = requests.get(url, cookies=a, timeout=15, headers=kv)
 r.encoding = "utf-8"
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(r.text,"html.parser")
-#print(soup.prettify())
 
-for ul in soup.find_all("ul",attrs={"class":"round"}):
+soup = BeautifulSoup(r.text, "html.parser")
+
+for ul in soup.find_all("ul", attrs={"class": "round"}):
     if ul:
         for tag in ul.find_all("li"):
             try:
-                print("正在处理章节" + tag.string + "\r",end ="")
-                char = tag.string.replace(" ","")
-                course = str(tag.attrs['data-courseid'])            
+                course = str(tag.attrs['data-courseid'])
                 geteidu = "http://www.chinesemooc.org/course.php?ac=course_live&op=live&course_id=" + course
-                o = requests.get(geteidu,cookies = a,timeout = 15,headers = kv)
-                o.encoding = "utf-8"
-                oorg = o.text
-                oeid = re.search(r'eid.*?,',oorg)[0]
-                name = re.search(r'course_name.*?,',oorg)[0]
-                name = name.replace('course_name":"',"")
-                name = name.replace('",',"")
-                eid = re.search(r'[0-9]{5}',oeid)[0]
-                furl = "http://www.chinesemooc.org/api/course_video_watch.php?course_id=" + course + "&eid=" + eid
-                p = requests.get(furl,cookies = a,timeout = 15,headers = kv)
-                p.encoding = "utf-8"
-                forg = p.text
-                durl = re.search(r'http.*?\.mp4',forg)[0]
-                durl = durl.replace("\\","")
-                durl = durl.replace(".mp4","_HD.mp4")
-                with open('data.csv', 'ab+') as f:
-                    title = [char,name,durl]
-                    line = ','.join(title) + '\n'
-                    f.write(line.encode('UTF-8'))
-                if de != 'N':
-                    print("延时等待    " + tag.string + "\r",end ="")
-                    time.sleep(3)
+                obj = requests.get(geteidu, cookies=a, timeout=15, headers=kv)
+                obj.encoding = "utf-8"
+                obj_content = obj.text
+                obj_eid_list = re.findall(r'eid.*?\d\"', obj_content)
+
+                for index in range(len(obj_eid_list)):
+                    char = tag.string.replace(" ", "") + "." + str(index + 1)
+                    print("正在处理章节" + char + "\n", end="")
+
+                    eid = re.search(r'\d{5}', obj_eid_list[index]).string
+                    eid = eid.replace("eid\":\"", "")
+                    eid = eid.replace("\"", "")
+                    furl = "http://www.chinesemooc.org/api/course_video_watch.php?course_id=" + course + "&eid=" + eid
+                    page = requests.get(furl, cookies=a, timeout=15, headers=kv)
+                    page.encoding = "utf-8"
+                    page_content = page.text
+                    obj_url = re.search(r'mp4_url.*?mp4\"', page_content)
+                    durl = obj_url[0].replace("mp4_url\":\"", "")
+                    durl = durl.replace("\\", "")
+                    durl = durl.replace("\"", "")
+                    with open('data.csv', 'ab+') as f:
+                        title = [char, durl]
+                        line = ','.join(title) + '\n'
+                        f.write(line.encode('UTF-8'))
+                    if de != 'N':
+                        sleep_second = 3
+                        print("延时等待 " + str(sleep_second) + " 秒…\n", end="")
+                        time.sleep(sleep_second)
             except:
-                print("在获取章节" + tag.string.replace(" ","") + "过程中出现了些问题，可能是网络连接超时或本章节无视频，错误将被忽略并继续")
+                print("在获取章节 " + char + " 过程中出现了些问题，可能是网络连接超时或本章节无视频，错误将被忽略并继续")
                 continue
 print("\n完成！感谢使用\n")
 input("现在可以关闭这个程序或按回车退出")
--- ren.py
+++ ren.py
@@ -14,21 +14,18 @@ print(" -------------------------------------------------------------- \n\n")
 import csv
 import os
 import re
+
 csv_reader = csv.reader(open('data.csv', encoding='utf-8'))
-i = 0
-input("\n按回车开始\n")
+lesson = input("请输入课程名以开始：\n")
+i = -1
 for row in csv_reader:
-    #print(row)
     i = i + 1
     try:
-        old = re.search(r'/[0-9]*-[0-9]*.*',row[2])[0][1:]
-        new = str(i) + ".mp4"
-        print("尝试把" + old + "重命名为" +new)
-        os.rename(old,new)
+        old = re.search(r'/\d+-\d+\.mp4', row[1])[0][1:]
+        new = lesson + "-" + row[0] + ".mp4"
+        print("尝试把 " + old + " 重命名为 " + new)
+        os.rename(old, new)
     except:
         continue
-    
 
 input("处理完成，按回车键结束程序")
-
-
